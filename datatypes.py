0d33b655b27fe1547f48c8ef0ec50f918dd3f67e
Murilo <murilok.andrade@gmail.com>
2026-08-19 13:27:06 -0300

    Homerwork 4

---
+++ datatypes.py
@@ -0,0 +1,10 @@
+Age=int(input("Please enter your age: "))
+print("Your age is:", Age)
+
+name=input("Please enter your name:")
+print("Your name is:", name)
+
+Pizza=input(" Which pizza do you want?")
+print("Your", Pizza, "Is coming...🍕")
+#Murilo   
+print(name[1:5])
