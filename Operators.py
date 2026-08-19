d45ecbc36db23a0abaac8a9f09e064eb6639c54b
Murilo <murilok.andrade@gmail.com>
2026-08-19 14:49:05 -0300

    Class 5

---
+++ Operators.py
@@ -0,0 +1,17 @@
+chocolate=20
+kids=4
+
+total= chocolate+5
+print("Total chocolate:", total)
+chocolate+=5
+print("Chocolate after adding 5:", chocolate)
+remaining= total-1
+print("Remaining chocolate:", remaining)
+chocolate-=2
+print("Remaining chocolate:", chocolate)
+
+print("Chocolate is greater than or equal to 2:", chocolate>=2)
+print("Chocolate is less than or equal to 100:", chocolate<=100)
+print("Total chocolate is less than or equal to 100:", total<=100)
+
+
