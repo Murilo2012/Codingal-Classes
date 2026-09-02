7507da80c971ded2b773c43af75fcab0b4974a98
Murilo <murilok.andrade@gmail.com>
2026-09-02 14:45:31 -0300

    Class 6

---
+++ nestedcondicions.py
@@ -0,0 +1,28 @@
+homework=True
+weather="sunny"
+
+if homework and weather=="sunny":
+    print("You can go out and play")
+elif homework and weather!="sunny":
+    print("You need to stay home and do your homework")
+else:
+    print("You need to stay home and do your homework")
+
+marks=75
+
+if marks>=90:
+    print("good")
+elif marks>=60:
+    print("good")
+else:
+    print("You need to practice")
+
+
+ homework=input("Is homework done? (yes/no): ")
+money=input("Do you have money? (yes/no): ")   
+parents=input("Did parents allow? (yes/no): ")
+
+If homework=="yes" and money=="yes" and parents=="yes":
+    print("You can go out and play")
+else:
+    print("You need to stay home") 
