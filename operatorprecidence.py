1b3154ded4c1f8997d12523d52cfe0f823d7123a
Murilo <murilok.andrade@gmail.com>
2026-09-16 14:37:39 -0300

    Class 8

---
+++ operatorprecidence.py
@@ -0,0 +1,28 @@
+V=4 
+W=5
+X=8
+Y=2
+Z=0
+A=2
+Z=(V+W)*X/Y
+print("Value of operator precidence", Z)
+
+
+
+Z=A*(X+Y)*Y
+print("The new value of operator precidence", Z)
+#0+2*(8+2)*2
+#2*10*2
+#20*2
+#40 <-- Answer 
+
+name="Elix"
+age=0
+
+if name=="Elix" or name=="John" and age>=2:
+    print("You are allowed to enter")
+else:
+    print("Goodbye")    
+# name= ok,  age= false
+
+
